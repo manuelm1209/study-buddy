26872f3e850d24a15f40d33c8674233c19c09add
Manuel Montoya <manuelm1209@hotmail.com>
2024-11-28 15:50:47 -0500

    Initial working code

--- main.py
+++ main.py
@@ -14,42 +14,74 @@ client = openai.OpenAI()
 
 model = "gpt-4o-mini"
 
+# Create a vector store caled "Test Vectore Store"
+# vector_store = client.beta.vector_stores.create(name="Test Vectore Store")
+
 # Step 1. Upload a file to OpenAI embeddings ===
-filepath = "./cryptocurrency.pdf"
-file_object = client.files.create(file=open(filepath, "rb"), purpose="assistants")
+file_path = ["./cryptocurrency.pdf"]
+file_streams = [open(path, "rb") for path in file_path]
+
+# Upload the user provided file to OpenAI
+# file_path_2 = "./AI_Program_Brochure.pdf"
+# file_object = client.files.create(
+#   file=open(file_path_2, "rb"), purpose="assistants"
+# )
+
+# Use the upload and poll SDK helper to upload the files, add them to the vector store,
+# and poll the status of the file batch for completion.
+# file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+#   vector_store_id=vector_store.id, files=file_streams
+# )
+
+# You can print the status and the file counts of the batch to see the result of this operation.
+# print(file_batch.status)
+# print(file_batch.file_counts)
 
 
 # Step 2. Create an assistant
-assistant = client.beta.assistants.create(
-    name="Study Buddy",
-    instructions="""You are a helpful study assistant who knows a lot about understanding research papers.
-    Your role is to summarize papers, clarify terminology within context, and extract key figures and data.
-    Cross-reference information for additional insights and answer related questions comprehensively.
-    Analyze the papers, noting strengths and limitations.
-    Respond to queries effectively, incorporating feedback to enhance your accuracy.
-    Handle data securely and update your knowledge base with the latest research.
-    Adhere to ethical standards, respect intellectual property, and provide users with guidance on any limitations.
-    Maintain a feedback loop for continuous improvement and user support.
-    Your ultimate goal is to facilitate a deeper understanding of complex scientific material, making it more accessible and comprehensible.""",
-    tools=[{type: "retrieval"}],
-    model=model,
-    file_ids=[file_object.id] 
-)
+# assistant = client.beta.assistants.create(
+#     name="Study Buddy",
+#     instructions="""You are a helpful study assistant who knows a lot about understanding research papers.
+#     Your role is to summarize papers, clarify terminology within context, and extract key figures and data.
+#     Cross-reference information for additional insights and answer related questions comprehensively.
+#     Analyze the papers, noting strengths and limitations.
+#     Respond to queries effectively, incorporating feedback to enhance your accuracy.
+#     Handle data securely and update your knowledge base with the latest research.
+#     Adhere to ethical standards, respect intellectual property, and provide users with guidance on any limitations.
+#     Maintain a feedback loop for continuous improvement and user support.
+#     Your ultimate goal is to facilitate a deeper understanding of complex scientific material, making it more accessible and comprehensible.""",
+#     tools=[{"type": "file_search"}],
+#     model=model,
+# )
 
-# === Get the Assistant ID ===
-assis_id = assistant.id
-print(assis_id)
+# # === Update the assistant to use the new Vector Store ===
+# assis_id = assistant.id
+# vector_store_id = vector_store.id
+# print(f"Assistant ID: {assis_id}")
+# print(f"Vector Store ID: {vector_store_id}")
+
+# assistant = client.beta.assistants.update(
+#   assistant_id=assistant.id,
+#   tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+# )
 
 # == Hardcoded ids to be used once the first code run is done and the assistant was created
-# thread_id = ""
-# assis_id = ""
+assis_id = "asst_Xr0FXRBhGUjAiURw2YkgnrdU"
+thread_id = "thread_OXNU4QTfQt5XVNzGogD4JReV"
 
-# === Step 3. Create a Thread
-message = "What is my name?"
+# # === Step 3. Create a Thread
+message = "What is mining?"
+message = client.beta.threads.messages.create(thread_id=thread_id, role="user", content=message)
 
-thread = client.beta.threads.create()
-thread_id = thread.id
-print(thread_id)
+# thread = client.beta.threads.create(
+#     messages=[{
+#         "role":"user",
+#         "content":message,
+#         # Attach the new file to the message.
+#         "attachments": [{ "file_id": file_object.id, "tools": [{"type": "file_search"}]}],
+# }])
+# thread_id = thread.id
+# print(f"Thread ID: {thread_id}")
 
 # === Run the Assistant
 run = client.beta.threads.runs.create(
@@ -60,33 +92,30 @@ run = client.beta.threads.runs.create(
 
 
 def wait_for_run_completion(client, thread_id, run_id, sleep_interval=5):
-    """
-    Waits for a run to complete and prints the elapsed time.:param client: The OpenAI client object.
-    :param thread_id: The ID of the thread.
-    :param run_id: The ID of the run.
-    :param sleep_interval: Time in seconds to wait between checks.
-    """
-    while True:
-        try:
-            run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
-            if run.completed_at:
-                elapsed_time = run.completed_at - run.created_at
-                formatted_elapsed_time = time.strftime(
-                    "%H:%M:%S", time.gmtime(elapsed_time)
-                )
-                print(f"Run completed in {formatted_elapsed_time}")
-                logging.info(f"Run completed in {formatted_elapsed_time}")
-                # Get messages here once Run is completed!
-                messages = client.beta.threads.messages.list(thread_id=thread_id)
-                last_message = messages.data[0]
-                response = last_message.content[0].text.value
-                print(f"Assistant Response: {response}")
+    if thread_id and run_id:
+        while True:
+            try:
+                run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
+                if run.completed_at:
+                    elapsed_time = run.completed_at - run.created_at
+                    formatted_elapsed_time = time.strftime(
+                        "%H:%M:%S", time.gmtime(elapsed_time)
+                    )
+                    print(f"Run completed in {formatted_elapsed_time}")
+                    logging.info(f"Run completed in {formatted_elapsed_time}")
+                    # Get messages here once Run is completed!
+                    messages = client.beta.threads.messages.list(thread_id=thread_id)
+                    last_message = messages.data[0]
+                    response = last_message.content[0].text.value
+                    print(f"Assistant Response: {response}")
+                    # st.markdown("#### MESSAGES: ")
+                    # st.write(messages)
+                    break
+            except Exception as e:
+                logging.error(f"An error occurred while retrieving the run: {e}")
                 break
-        except Exception as e:
-            logging.error(f"An error occurred while retrieving the run: {e}")
-            break
-        logging.info("Waiting for run to complete...")
-        time.sleep(sleep_interval)
+            logging.info("Waiting for run to complete...")
+            time.sleep(sleep_interval)
         
 # === Run it
 wait_for_run_completion(client=client,
@@ -94,6 +123,10 @@ wait_for_run_completion(client=client,
                         run_id=run.id)
 
 # === Check the Run Steps - LOGS ===
-run_steps = client.beta.threads.runs.list(thread_id=thread_id, run_id=run.id)
+run_steps = client.beta.threads.runs.steps.list(thread_id=thread_id, run_id=run.id)
 print(f"\nRun Steps:::>>> {run_steps.data[0]}")
 
+thread_retrieve = client.beta.threads.retrieve(thread_id)
+print(f"\nThread retreaved: {thread_retrieve}")
+
+# st.write(thread_retrieve)
